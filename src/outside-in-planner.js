9a3e220d54b8c8ceafb960892d146962f2aae0d6
mateofoulkes-lab <mateofoulkes@gmail.com>
2026-09-14 10:01:53 -0300

    Expose removable frontier for automatic carving

--- src/outside-in-planner.js
+++ src/outside-in-planner.js
@@ -12,6 +12,25 @@ export class OutsideInPlanner {
     this.targetField = null;
   }
 
+  isCellRemovable(x, y, z) {
+    if (!this.stock.solid(x, y, z)) return false;
+    const key = this.stock.key(x, y, z);
+    if (!this.stock.surface.has(key)) return false;
+    if (!this.targetField) return true;
+    const p = this.stock.toLocal(x, y, z, this.stock.p);
+    return this.targetField.isDefinitelyOutside(p);
+  }
+
+  getRemovableSurfaceCells() {
+    const cells = [];
+    this.stock.forEachSurface((x, y, z, p) => {
+      if (!this.targetField || this.targetField.isDefinitelyOutside(p)) {
+        cells.push({ x, y, z, key: this.stock.key(x,y,z), point: p.clone() });
+      }
+    });
+    return cells;
+  }
+
   carveSphere(localPoint, radius) {
     const target = this.targetField;
     return this.stock.removeSurfaceSphere(
